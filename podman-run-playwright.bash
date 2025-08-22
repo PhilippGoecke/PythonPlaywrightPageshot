7feb23fafc9fda31f4b446ab2deb16a9c45acc9c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-23 01:04:15 +0200

    Update podman-run-playwright.bash

--- podman-run-playwright.bash
+++ podman-run-playwright.bash
@@ -1,2 +1,2 @@
 podman build --no-cache --rm -f Containerfile -t playwright:pageshot .
-podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot "source bin/activate && python pageshot.py 'https://podman.io/'"
+podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot "pageshot.py 'https://podman.io/'"
