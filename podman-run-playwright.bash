b9222cea839da1000a7f02068b668bfbb95d488d
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-23 00:16:33 +0200

    Modify podman run command for Playwright

--- podman-run-playwright.bash
+++ podman-run-playwright.bash
@@ -1,2 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t playwright:pageshot .
-podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot
+#podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot
+podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot python pageshot.py "https://playwright.dev"
