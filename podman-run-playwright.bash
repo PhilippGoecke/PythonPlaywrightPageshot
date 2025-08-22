c29278459aaf96ddbc7ffad821dd98e31dbd61d6
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-23 00:38:31 +0200

    Update podman command to use new URL and activate script

--- podman-run-playwright.bash
+++ podman-run-playwright.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t playwright:pageshot .
 #podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot
-podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot python pageshot.py "https://playwright.dev"
+podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot "source bin/activate && python pageshot.py 'https://podman.io/'"
