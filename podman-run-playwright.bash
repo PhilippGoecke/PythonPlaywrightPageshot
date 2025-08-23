b47ecca2ea9e8defa4e15069b920deef3dcb5f1f
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-08-23 02:25:33 +0200

    Change command to use python3 for pageshot script

--- podman-run-playwright.bash
+++ podman-run-playwright.bash
@@ -1,2 +1,2 @@
 podman build --no-cache --rm -f Containerfile -t playwright:pageshot .
-podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot "pageshot.py 'https://podman.io/'"
+podman run --interactive --tty -v ./data:/playwright/data playwright:pageshot "python3 pageshot.py 'https://podman.io/'"
